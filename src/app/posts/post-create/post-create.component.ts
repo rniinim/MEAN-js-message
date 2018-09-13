53ad5732cc2f6ca9bbd25fe32799fd4990e530e5
rniinim <r.niinimaki@windowslive.com>
2018-09-13 20:17:12 +0300

    angular components and upgrades

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent{
+export class PostCreateComponent {
+  newPost = 'NO CONTENT';
+  enteredValue = '';
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
 }
