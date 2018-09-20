181d5d53d790c2d2ba8742ce62f03665e536a591
rniinim <r.niinimaki@windowslive.com>
2018-09-20 23:13:03 +0300

    modified post-list component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {
-      title: 'First Post',
-      content: 'this is the first post content'
-    },
-    {
-      title: 'Second Post',
-      content: 'this is the second post content'
-    },
-    {
-      title: 'Third Post',
-      content: 'this is the third post content'
-    }
-  ];
+  @Input() posts = [];
 }
