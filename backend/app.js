f50fe4d6ac6dc7d909b1457c89effaee761903c1
rniinim <r.niinimaki@windowslive.com>
2018-10-13 00:59:10 +0300

    edtited backend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added'
+  });
 });
 
-module.exports = app;
+app.use('/api/posts' ,(req, res, next) => {
+  const posts= [
+    {
+      id: 'fdasf323',
+      title: 'Server-side post',
+      content:'coming from server'
+    },
+    {
+      id: 'dasf342',
+      title: 'Server-side post 2',
+      content:'coming from server 2'
+    },
+    {
+      id: 'fds32423',
+      title: 'Server-side post 3',
+      content:'coming from server 3'
+    }
+  ];
+  res.status(200).json({
+    message: 'posts ferched',
+    posts: posts
+  });
+});
+
+module.exports = app; 
